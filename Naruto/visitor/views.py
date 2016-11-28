1aef865fd5207a7a951d381daf54a48ec96d4a04
an9wer <an9wer@gmail.com>
2016-11-28 18:53:50 +0800

    add pages for visitor

--- Naruto/visitor/views.py
+++ Naruto/visitor/views.py
@@ -1,6 +1,20 @@
 from . import visitor
-from flask import render_template
+from flask import request, render_template
+from ..models import Post
 
-@visitor.route('/index')
+@visitor.route('/')
 def index():
-	return render_template('base.html')
+	print(request.headers)
+	page = request.args.get('page', 1, type=int)
+	pagination = Post.query.order_by(Post.publish_time.desc()).paginate(page=page, per_page=10, error_out=False)
+	posts = pagination.items
+	return render_template('visitor/index.html',
+						   page=page,
+						   pagination=pagination,
+						   posts=posts)
+
+@visitor.route('/post/<int:post_id>')
+def show_post(post_id):
+	post = Post.query.filter_by(id=post_id).first()
+	return render_template('visitor/show_post.html',
+						   post=post)
